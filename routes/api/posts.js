7728bb1ea056b68fe44e5b22a4c75c9ae216fa44
Andrew Westerman <awesterman1507@gmail.com>
2020-08-11 18:21:55 -0700

    Added ability to add and remove comments on posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -161,4 +161,74 @@ router.put('/:post_id/unlike', auth, async (req, res) => {
     }
 });
 
+// @route   POST api/posts/:post_id/comments
+// @desc    Comment on a post
+// @access  Private
+router.post(
+    '/:post_id/comments',
+    [auth, check('text', 'Text is required').notEmpty()],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+
+            let post = await Post.findById(req.params.post_id);
+            // Check if post exists
+            if (!post) return res.status(404).json({ msg: 'Post not found' });
+
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id,
+            };
+
+            post.comments.push(newComment);
+
+            post = await post.save();
+            return res.json(post.comments);
+        } catch (err) {
+            console.error(err.message);
+            if (err.kind === 'ObjectId') {
+                return res.status(404).json({ msg: 'Post not found' });
+            }
+            return res.status(500).send('Server Error');
+        }
+    }
+);
+
+// @route   POST api/posts/:post_id/comments/:com_id
+// @desc    Remove comment on a post
+// @access  Private
+router.delete('/:post_id/comments/:com_id', auth, async (req, res) => {
+    try {
+        let post = await Post.findById(req.params.post_id);
+        // Check if post exists
+        if (!post) return res.status(404).json({ msg: 'Post not found' });
+
+        const comment = post.comments.find((c) => c.id === req.params.com_id);
+        // Check if comment id exists
+        if (!comment) return res.status(404).json({ msg: 'Comment not found' });
+        // Check if user is authorized to delete comment
+        if (comment.user.toString() !== req.user.id)
+            return res.status(401).json({ msg: 'Not authorized' });
+
+        // Delete the comment
+        post.comments = post.comments.filter((c) => c.id !== req.params.com_id);
+
+        post = await post.save();
+        return res.json(post.comments);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        return res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
