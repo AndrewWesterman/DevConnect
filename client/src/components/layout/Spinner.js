261ed2d78256d5073db8217981796f8b201dddaf
Andrew Westerman <awesterman1507@gmail.com>
2020-08-12 19:12:06 -0700

    Added loading spinner component

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+export default () => (
+    <Fragment>
+        <div className='lds-ring'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </Fragment>
+);
