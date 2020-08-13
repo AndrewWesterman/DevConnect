a008ac28bc945956cffeb27a3f1de06c5f9f1c24
Andrew Westerman <awesterman1507@gmail.com>
2020-08-13 14:20:17 -0700

    Fixed a warning

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,6 +4,7 @@ import { setAlert } from './alert';
 import {
     GET_PROFILE,
     GET_PROFILES,
+    GET_REPOS,
     PROFILE_ERROR,
     UPDATE_PROFILE,
     CLEAR_PROFILE,
@@ -264,7 +265,7 @@ export const deleteEducation = (edu_id) => async (dispatch) => {
 export const deleteAccount = () => async (dispatch) => {
     if (window.confirm('Are you sure? This action cannot be undone...')) {
         try {
-            const res = await axios.delete(`/api/profile`);
+            await axios.delete(`/api/profile`);
             dispatch({ type: CLEAR_PROFILE });
             dispatch({ type: ACCOUNT_DELETED });
 
