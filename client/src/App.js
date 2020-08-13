1efb7a5daa0962d7e49f5e2b44d6c8e1aea8e0fb
Andrew Westerman <awesterman1507@gmail.com>
2020-08-12 21:40:09 -0700

    Renamed CreateProfile to ProfileForm and refactored to allow edits

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/profile-forms/CreateProfile';
+import ProfileForm from './components/profile-forms/ProfileForm';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import { Provider } from 'react-redux';
@@ -50,7 +50,12 @@ const App = () => {
                             <PrivateRoute
                                 exact
                                 path='/create-profile'
-                                component={CreateProfile}
+                                component={ProfileForm}
+                            />
+                            <PrivateRoute
+                                exact
+                                path='/edit-profile'
+                                component={ProfileForm}
                             />
                         </Switch>
                     </section>
--- client/src/components/profile-forms/ProfileForm.js
+++ client/src/components/profile-forms/ProfileForm.js
@@ -1,27 +1,50 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { createProfile } from '../../actions/profile';
+import { createProfile, getCurrentProfile } from '../../actions/profile';
 
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-export const CreateProfile = ({ createProfile, history }) => {
-    const [formData, setFormData] = useState({
-        company: '',
-        website: '',
-        location: '',
-        status: '',
-        skills: '',
-        githubusername: '',
-        bio: '',
-        twitter: '',
-        facebook: '',
-        linkedin: '',
-        youtube: '',
-        instagram: '',
-    });
+const initialState = {
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: '',
+    githubusername: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    youtube: '',
+    instagram: '',
+};
+
+export const ProfileForm = ({
+    profile: { profile, loading },
+    createProfile,
+    getCurrentProfile,
+    history,
+}) => {
+    const [formData, setFormData] = useState(initialState);
 
     const [showSocials, toggleSocials] = useState(false);
+
+    useEffect(() => {
+        if (!profile) getCurrentProfile();
+        if (!loading && profile) {
+            const profileData = { ...initialState };
+            for (const key in profile) {
+                if (key in profileData) profileData[key] = profile[key];
+            }
+            for (const key in profile.social) {
+                if (key in profileData) profileData[key] = profile.social[key];
+            }
+            if (Array.isArray(profileData.skills))
+                profileData.skills = profileData.skills.join(', ');
+            setFormData(profileData);
+        }
+    }, [loading, getCurrentProfile, profile]);
 
     const {
         company,
@@ -43,12 +66,19 @@ export const CreateProfile = ({ createProfile, history }) => {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        createProfile(formData, history);
+        createProfile(formData, history, profile ? true : false);
     };
 
     return (
         <Fragment>
-            <h1 className='large text-primary'>Create Your Profile</h1>
+            <h1 className='large text-primary'>
+                {profile ? (
+                    <Fragment>Edit</Fragment>
+                ) : (
+                    <Fragment>Create</Fragment>
+                )}{' '}
+                Your Profile
+            </h1>
             <p className='lead'>
                 <i className='fas fa-user'></i> Let's get some information to
                 make your profile stand out
@@ -241,8 +271,16 @@ export const CreateProfile = ({ createProfile, history }) => {
     );
 };
 
-CreateProfile.propTypes = {
+ProfileForm.propTypes = {
     createProfile: PropTypes.func.isRequired,
+    getCurrentProfile: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
 };
 
-export default connect(null, { createProfile })(withRouter(CreateProfile));
+const mapStateToProps = (state) => ({
+    profile: state.profile,
+});
+
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(
+    ProfileForm
+);
